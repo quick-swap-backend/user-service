f9a2e7357c48ccb59d8bd4b6cfe3ab0d5d6f7ca8
최준혁 <cwnsgur13579@gmail.com>
2025-10-18 10:09:20 +0900

    프로젝트 초기화

---
+++ src/test/kotlin/quickswap/userservice/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package quickswap.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
